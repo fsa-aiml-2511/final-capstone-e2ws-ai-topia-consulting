9b33f0098a97acbf24ff8c820c2852805345344a
Sam Reid <sreid@algobull.ai>
2026-05-19 22:30:31 -0400

    Fix Streamlit app wrapper path

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ from pathlib import Path
 import runpy
 
 
-runpy.run_path(Path(__file__).parent / "webapp" / "app.py", run_name="__main__")
+runpy.run_path(str(Path(__file__).parent / "webapp" / "app.py"), run_name="__main__")
